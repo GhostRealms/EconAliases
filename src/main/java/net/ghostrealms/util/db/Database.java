ff5b062642dc24ccf8e7cc6ce108385bdeefd257
River Marmorstein <river@rmarmorstein.com>
2016-08-11 02:09:43 -0400

    using the much better Realms DB class, not the bukkit resources section one

--- src/main/java/net/ghostrealms/util/db/Database.java
+++ src/main/java/net/ghostrealms/util/db/Database.java
@@ -1,125 +1,240 @@
 package net.ghostrealms.util.db;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 
-/**
- * Abstract Database class, serves as a base for any connection method (MySQL,
- * SQLite, etc.)
- *
- */
+public class Database {
 
-public abstract class Database {
+    public enum SQL {
+        H2,
+        MYSQL,
+        SQLITE;
+    }
 
-    protected Connection connection;
+    public static int test;
 
-    /**
-     * Creates a new Database
-     *
-     */
-    protected Database() {
-        this.connection = null;
+    private final JavaPlugin plugin;
+    private final String db;
+    private SQL mode;
+
+    private Connection connection;
+
+    private String mysql_user;
+    private String mysql_pass;
+    private String mysql_host;
+    private String mysql_database;
+    private int    mysql_port;
+
+    private ArrayList<String> updateQueue = new ArrayList<String>();
+
+    public Database(String db, JavaPlugin plugin, SQL mode) {
+        this.db = db;
+        this.plugin = plugin;
+        this.mode = mode;
+
+        //make sure config exists
+        File configFile = new File(plugin.getDataFolder() + File.separator + "config.yml");
+        if(!configFile.exists())
+        {
+            try {
+                configFile.createNewFile();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        if(mode == SQL.MYSQL) {
+            FileConfiguration config = plugin.getConfig();
+
+            if(!config.contains("mysql.database"))
+                config.set("mysql.database", "dbname");
+            if(!config.contains("mysql.host"))
+                config.set("mysql.host", "localhost");
+            if(!config.contains("mysql.user"))
+                config.set("mysql.user", "root");
+            if(!config.contains("mysql.pass"))
+                config.set("mysql.pass", "password");
+            if(!config.contains("mysql.port"))
+                config.set("mysql.port", 3306);
+
+            try
+            {
+                config.save(configFile);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            mysql_database = config.getString("mysql.database");
+            mysql_host     = config.getString("mysql.host");
+            mysql_user     = config.getString("mysql.user");
+            mysql_pass     = config.getString("mysql.pass");
+            mysql_port     = config.getInt("mysql.port");
+        }
+
+        setupConnection();
+    }
+
+    private void setupConnection() {
+
+        switch(mode) {
+            default:
+            case H2:
+                try {
+                    Class.forName("org.h2.Driver");
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                    connection = null;
+                    break;
+                }
+
+                String url = "jdbc:h2:" + plugin.getDataFolder() + File.separator + db;
+
+                try {
+                    connection = DriverManager.getConnection(url);
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case MYSQL:
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                    connection = null;
+                    break;
+                }
+
+                try {
+                    connection = DriverManager.getConnection("jdbc:mysql://" + mysql_host + ":" + mysql_port +
+                            "/" + mysql_database,  mysql_user, mysql_pass);
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    connection = null;
+                    break;
+                }
+                break;
+            case SQLITE:
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                    connection = null;
+                    break;
+                }
+
+                try {
+                    connection = DriverManager.getConnection("jdbc:sqlite:" + plugin.getDataFolder() +
+                            File.separator + db);
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    connection = null;
+                    break;
+                }
+                break;
+        }
+    }
+
+    protected String getDatabase() {
+        return db;
+    }
+
+    private String getDatabaseURL() {
+        return "jdbc:h2:" + plugin.getDataFolder() + File.separator + db;
+    }
+
+    public void close() {
+        try {
+            if(!connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
-     * Opens a connection with the database
-     *
-     * @return Opened connection
-     * @throws SQLException
-     *             if the connection can not be opened
-     * @throws ClassNotFoundException
-     *             if the driver cannot be found
+     * This will add an Update SQL statement to the queue to be updated upon calling #runQueue*
+     * @param statement
      */
-    public abstract Connection openConnection() throws SQLException,
-            ClassNotFoundException;
-
-    /**
-     * Checks if a connection is open with the database
-     *
-     * @return true if the connection is open
-     * @throws SQLException
-     *             if the connection cannot be checked
-     */
-    public boolean checkConnection() throws SQLException {
-        return connection != null && !connection.isClosed();
+    public void queue(String statement) {
+        updateQueue.add(statement);
     }
 
-    /**
-     * Gets the connection with the database
-     *
-     * @return Connection with the database, null if none
-     */
-    public Connection getConnection() {
-        return connection;
+    public void runQueue() {
+        for(String sql : updateQueue) {
+            update(sql);
+            updateQueue.remove(sql);
+        }
     }
 
-    /**
-     * Closes the connection with the database
-     *
-     * @return true if successful
-     * @throws SQLException
-     *             if the connection cannot be closed
-     */
-    public boolean closeConnection() throws SQLException {
-        if (connection == null) {
+    public boolean write(String sql, Object...args) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            for(int i = 0; i < args.length; i++)
+            {
+                //+1 because the first parameter is 1, not 0
+                stmt.setObject(i + 1, args[i]);
+            }
+
+            boolean status = stmt.execute();
+            return status;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
             return false;
         }
-        connection.close();
-        return true;
     }
 
+    public int update(String sql, Object...args) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
 
-    /**
-     * Executes a SQL Query<br>
-     *
-     * If the connection is closed, it will be opened
-     *
-     * @param query
-     *            Query to be run
-     * @return the results of the query
-     * @throws SQLException
-     *             If the query cannot be executed
-     * @throws ClassNotFoundException
-     *             If the driver cannot be found; see {@link #openConnection()}
-     */
-    public ResultSet querySQL(String query) throws SQLException,
-            ClassNotFoundException {
-        if (!checkConnection()) {
-            openConnection();
+            for(int i = 0; i < args.length; i++)
+            {
+                //+1 because the first parameter is 1, not 0
+                stmt.setObject(i + 1, args[i]);
+            }
+
+            int status = stmt.executeUpdate();
+            return status;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return -1;
         }
-
-        Statement statement = connection.createStatement();
-
-        ResultSet result = statement.executeQuery(query);
-
-        return result;
     }
 
-    /**
-     * Executes an Update SQL Query<br>
-     * See {@link java.sql.Statement#executeUpdate(String)}<br>
-     * If the connection is closed, it will be opened
-     *
-     * @param query
-     *            Query to be run
-     * @return Result Code, see {@link java.sql.Statement#executeUpdate(String)}
-     * @throws SQLException
-     *             If the query cannot be executed
-     * @throws ClassNotFoundException
-     *             If the driver cannot be found; see {@link #openConnection()}
-     */
-    public int updateSQL(String query) throws SQLException,
-            ClassNotFoundException {
-        if (!checkConnection()) {
-            openConnection();
+    public ResultSet read(String sql, Object...args) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+
+            for(int i = 0; i < args.length; i++)
+            {
+                //+1 because the first parameter is 1, not 0
+                stmt.setObject(i + 1, args[i]);
+            }
+
+            ResultSet results = stmt.executeQuery();
+            return results;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
         }
-
-        Statement statement = connection.createStatement();
-
-        int result = statement.executeUpdate(query);
-
-        return result;
     }
+
+    protected void setMode(SQL mode) {
+        this.mode = mode;
+    }
+
+    protected SQL getMode() {
+        return mode;
+    }
+
 }
